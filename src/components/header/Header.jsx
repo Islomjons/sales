70a19efabb9e2f586bf692f4fb5d0c8e096e1f55
Islomjons <mukhammadalievislomjon@gmail.com>
2023-06-02 03:43:49 +0500

    vcsdvs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,6 +18,23 @@ const Header = () => {
       <h1>islomjon Mukhammadaliev</h1>
       <h1>islomjon Mukhammadaliev</h1>
       <h1>islomjon Mukhammadaliev</h1>
+
+      <h2>lala</h2>
+      <h2>lala</h2>
+      <h2>lala</h2>
+
+      <h2>lala</h2>
+      <h2>lala</h2>
+
+      <h2>lala</h2>
+      <h2>lala</h2>
+      <h2>lala</h2>
+
+      <h2>lala</h2>
+      <h2>lala</h2>
+
+      <h2>lala</h2>
+      <h2>lala</h2>
     </div>
   )
 }
