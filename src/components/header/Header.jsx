edc6eb98fbfb5651c47ad7b58d43abbe994cbde4
Islomjons <mukhammadalievislomjon@gmail.com>
2023-08-26 23:50:03 +0500

    sasascacsdv

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Header.scss"
 
 const Header = () => {
+  const [fetch, setFetch] = useState([])
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+        .then(response => response.json())
+        .then(data => console.log(data))
+  }, [])
   return (
-    <div className="header"></div>
+    <div className="header">
+        
+    </div>
   )
 }
 
